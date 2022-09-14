98bc617359432ac1c60324be69fb44826ffa6ddd
Hugo Normand <hugo.normandcedano@gmail.com>
2022-09-14 20:44:25 +0200

    allez c'est fait

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: ' https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/'
+    baseURL: 'https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/'
 })
 
 export function postAPI() {
-    instance.post('/exec')
+    let post = {
+        cle: "CLE-TEST-IOT",
+        donnees: {
+            id: this.email,
+            date: JSON.stringify(this.date),
+            urlRelais: this.github,
+            message: this.motdoux
+        }
+    }
+    instance.post('/exec', post, {
+        headers: {
+            'Content-Type': 'application / json',
+        }
+    })
+    .then(() => console.log('Message envoyé'))
 }
